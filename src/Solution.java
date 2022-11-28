93f2ffd90acdfa2f89914bbf9fd69e3897d0bae1
turkeyzhou <turkeyzhou@tencent.com>
2022-11-28 15:42:24 +0800

    2022-11-28 part1

--- src/Solution.java
+++ src/Solution.java
@@ -2075,6 +2075,17 @@ public class Solution {
         }
     }
 
+    //给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (final int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
         final Solution solution = new Solution();
